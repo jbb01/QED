98441b2ad3ef76f6b48c84df018fd78a2e470590
jbb01 <32650546+jbb01@users.noreply.github.com>
2021-12-15 22:54:46 +0100

    migrated to gradle kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,98 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
+plugins {
+    id("com.android.application")
+    id("androidx.navigation.safeargs")
+}
+
+val localProperties = gradleLocalProperties(rootDir)
+
+android {
+    signingConfigs {
+        create("release") {
+            storeFile = file(localProperties.getProperty("KEY_STORE_FILE"))
+            storePassword = localProperties.getProperty("KEY_STORE_PASSWORD")
+            keyAlias = localProperties.getProperty("KEY_ALIAS")
+            keyPassword = localProperties.getProperty("KEY_PASSWORD")
+        }
+    }
+
+    compileSdk = 31
+
+    defaultConfig {
+        applicationId = "com.jonahbauer.qed"
+        versionCode = 1
+        versionName = "2.5.0-beta3"
+
+        minSdk = 24
+        targetSdk = 31
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables.useSupportLibrary = true
+    }
+
+    buildTypes {
+        getByName("release") {
+            postprocessing {
+                isRemoveUnusedCode = true
+                isObfuscate = false
+                isOptimizeCode = true
+            }
+
+            signingConfig = signingConfigs.getByName("release")
+        }
+
+        getByName("debug") {
+            isMinifyEnabled = false
+            isDebuggable = true
+        }
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+}
+
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
+    implementation("androidx.fragment:fragment:1.4.0")
+    implementation("androidx.navigation:navigation-fragment:2.3.5")
+    implementation("androidx.navigation:navigation-ui:2.3.5")
+    implementation("androidx.preference:preference:1.1.1")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.security:security-crypto:1.0.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("com.googlecode.json-simple:json-simple:1.1")
+    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation("com.x5dev:chunk-templates:3.6.1")
+    implementation("org.apache.commons:commons-io:1.3.2")
+    implementation("org.jsoup:jsoup:1.14.3")
+    implementation("com.google.crypto.tink:tink-android:1.6.1")
+    implementation("it.unimi.dsi:fastutil-core:8.5.6")
+    implementation("androidx.room:room-runtime:2.4.0")
+    implementation("androidx.room:room-rxjava3:2.4.0")
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
+
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
+
+    compileOnly("org.projectlombok:lombok:1.18.22")
+    annotationProcessor("org.projectlombok:lombok:1.18.22")
+
+    annotationProcessor("androidx.room:room-compiler:2.4.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:core:1.4.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test:runner:1.4.0")
+    androidTestImplementation("com.google.truth:truth:1.1.3")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+    }
+
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.10")
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
+    }
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+tasks {
+    create<Delete>("clean") {
+        delete(rootProject.buildDir)
+    }
+}
