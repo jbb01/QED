357e51675474b3d24858a593f88fff7b2903dac4
jbb01 <32650546+jbb01@users.noreply.github.com>
2022-04-01 18:00:15 +0200

    adjusted version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     defaultConfig {
         applicationId = "com.jonahbauer.qed"
         versionCode = 1
-        versionName = "2.5.0-beta3"
+        versionName = "2.5.0-beta4"
 
         minSdk = 24
         targetSdk = 31
